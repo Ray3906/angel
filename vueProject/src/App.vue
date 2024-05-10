1d18a07a53d51fa28c23b9b5412321a9a78a9ef9
Ray3906 <1535448513@qq.com>
2024-05-10 12:40:31 +0800

    init

---
+++ vueProject/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+
+ref
+</script>
+
+<template>
+  <div>
+    <h2>欢迎来到登录页面</h2>
+    <p>tips:</p>
+    <form action="#">
+      <input type="text" id="userName" ref="userName" placeholder="用户名">
+      <label for="text">密码</label>
+      <input type="password" id="password" placeholder="密码">
+      <input type="button" value="提交">
+    </form>
+  </div>
+</template>
